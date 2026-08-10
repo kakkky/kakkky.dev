6235aa2f848695a9b0066d822ec81363c9ef2c98
YutaKakiki <uta31501224@gmail.com>
2026-08-11 00:47:29 +0900

    update: TagBadge のコントラストを上げて視認性を改善

--- app/adapter/view/components/tag_badge_templ.go
+++ app/adapter/view/components/tag_badge_templ.go
@@ -29,7 +29,7 @@ func TagBadge(name string) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<span class=\"inline-flex items-center rounded-full border border-gray-200 bg-gray-50 px-2.5 py-0.5 text-xs text-gray-700\">#")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<span class=\"inline-flex items-center rounded-full border border-gray-300 bg-gray-100 px-2.5 py-0.5 text-xs font-medium text-gray-800\">#")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
